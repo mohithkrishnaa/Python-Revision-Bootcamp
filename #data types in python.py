ec3c4507a564b96dae04c620917e9253ee963035
Mohith Krishna <149135280+mohithkrishnaa@users.noreply.github.com>
2024-06-06 21:52:28 +0530

    Add files via upload

---
+++ #data types in python.py
@@ -0,0 +1,26 @@
+#data types in python
+
+# Integer
+x = 10
+print(x)  # Output: 10
+print(type(x))  # Output: <class 'int'>
+
+# Float
+y = 10.5
+print(y)  # Output: 10.5
+print(type(y))  # Output: <class 'float'>
+
+# String
+s = "Hello, World!"
+print(s)  # Output: Hello, World!
+print(type(s))  # Output: <class 'str'>
+
+# List
+my_list = [1, 2, 3, 4, 5]
+print(my_list)  # Output: [1, 2, 3, 4, 5]
+print(type(my_list))  # Output: <class 'list'>
+
+# Tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)  # Output: (1, 2, 3, 4, 5)
+print(type(my_tuple))  # Output: <class 'tuple'>
