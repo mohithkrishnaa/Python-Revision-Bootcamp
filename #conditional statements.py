5a7fc813be01b72bae111ba7fa4fa44e76e829b7
Mohith Krishna <149135280+mohithkrishnaa@users.noreply.github.com>
2024-06-06 21:51:38 +0530

    Add files via upload

---
+++ #conditional statements.py
@@ -0,0 +1,9 @@
+# Conditional statements
+age = 20
+
+if age < 20:
+    print("You are under 20 years old")
+elif age > 20:
+    print("You are above 20 years old")
+else:
+    print("You are 20 years old")
